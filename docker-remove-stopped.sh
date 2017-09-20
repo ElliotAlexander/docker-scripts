33bd9404f5d1fb1fc5d3566484133b0cac2a6662
ElliotAlexander <epurvis97@gmail.com>
2017-09-20 03:19:09 +0200

    Added root check to docker-remove-stopped

--- docker-remove-stopped.sh
+++ docker-remove-stopped.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ "$EUID" != 0 ]
+  then echo "Run me as root!"
+  exit
+fi
+
+
 docker rm $(docker ps -a -q)
 echo "Complete."
 echo "Use docker -a - q to view stopped containers."
